3b8f7e7fb4cb16349d7d2e62c18310b214c46319
Vishakh <vishakh@yahoo.com>
2015-10-11 05:29:31 +0000

    Contract declaration precedence

--- contracts/FeedBackedCall.sol
+++ contracts/FeedBackedCall.sol
@@ -1,5 +1,43 @@
 import "std.sol";
 
+
+contract PriceFeedApi {
+
+    // block time when the prices were last updated
+    uint public updateTime;
+
+    // returns the price of an asset
+    // the price is represented as uint: (double price) * 1000000
+    function getPrice(bytes32 symbol) returns(uint currPrice);
+
+    // returns the timestamp of the latest price for an asset
+    // normally this is the exchange timestamp, but if exchange
+    // doesn't supply such info the latest data retrieval time is returned
+    function getTimestamp(bytes32 symbol) returns(uint timestamp);
+}
+
+
+// Feed names:
+// USD_ETH  (ETH/USD)
+// BTC_ETH  (BTC/ETH)
+// USDT_BTC (USD/BTC)
+// EURUSD   (EUR/USD)
+// GBPUSD   (GBP/USD)
+// USDJPY   (USD/JPY)
+// XAUUSD   (Gold)
+// XAGUSD   (Silver)
+// SP500    (S&P 500)
+// NASDAQ   (NASDAQ)
+// AAPL     (Apple)
+// GOOG     (Google)
+// MSFT     (Microsoft)
+// GM       (General Motors)
+// GE       (General Electric)
+// WMT      (Walmart)
+// F        (Ford Motor)
+// T        (AT&T)
+
+
 contract FeedBackedCall is nameRegAware {
 
     struct StrikeCondition {
@@ -119,40 +157,3 @@ contract FeedBackedCall is nameRegAware {
         }
     }
 }
-
-
-contract PriceFeedApi {
-
-    // block time when the prices were last updated
-    uint public updateTime;
-
-    // returns the price of an asset
-    // the price is represented as uint: (double price) * 1000000
-    function getPrice(bytes32 symbol) returns(uint currPrice);
-
-    // returns the timestamp of the latest price for an asset
-    // normally this is the exchange timestamp, but if exchange
-    // doesn't supply such info the latest data retrieval time is returned
-    function getTimestamp(bytes32 symbol) returns(uint timestamp);
-}
-
-
-// Feed names:
-// USD_ETH  (ETH/USD)
-// BTC_ETH  (BTC/ETH)
-// USDT_BTC (USD/BTC)
-// EURUSD   (EUR/USD)
-// GBPUSD   (GBP/USD)
-// USDJPY   (USD/JPY)
-// XAUUSD   (Gold)
-// XAGUSD   (Silver)
-// SP500    (S&P 500)
-// NASDAQ   (NASDAQ)
-// AAPL     (Apple)
-// GOOG     (Google)
-// MSFT     (Microsoft)
-// GM       (General Motors)
-// GE       (General Electric)
-// WMT      (Walmart)
-// F        (Ford Motor)
-// T        (AT&T)
