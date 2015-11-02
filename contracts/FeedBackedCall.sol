1938a12741d5978cd682184cce7ecd64603771b0
Kapil Thadani <kapil.thadani@gmail.com>
2015-11-02 00:19:49 +0000

    Simplified internals and auth/exercise flow

--- contracts/FeedBackedCall.sol
+++ contracts/FeedBackedCall.sol
@@ -1,103 +1,67 @@
 import "std.sol";
+import "PriceFeedApi.sol";
 import "TradingAccount.sol";
-
-
-contract PriceFeedApi {
-    // block time when the prices were last updated
-    uint public updateTime;
-
-    // returns the price of an asset
-    // the price is represented as uint: (double price) * 1000000
-    function getPrice(bytes32 symbol) returns(uint currPrice);
-
-    // returns the timestamp of the latest price for an asset
-    // normally this is the exchange timestamp, but if exchange
-    // doesn't supply such info the latest data retrieval time is returned
-    function getTimestamp(bytes32 symbol) returns(uint timestamp);
-}
-
-
-// Feed names:
-// USD_ETH  (ETH/USD)
-// BTC_ETH  (BTC/ETH)
-// USDT_BTC (USD/BTC)
-// EURUSD   (EUR/USD)
-// GBPUSD   (GBP/USD)
-// USDJPY   (USD/JPY)
-// XAUUSD   (Gold)
-// XAGUSD   (Silver)
-// SP500    (S&P 500)
-// NASDAQ   (NASDAQ)
-// AAPL     (Apple)
-// GOOG     (Google)
-// MSFT     (Microsoft)
-// GM       (General Motors)
-// GE       (General Electric)
-// WMT      (Walmart)
-// F        (Ford Motor)
-// T        (AT&T)
 
 
 contract FeedBackedCall is nameRegAware {
 
-    struct StrikeCondition {
-        bytes32     feedName;       // feed to get prices
-        uint        strikePrice;    // strike price
-        uint        maturityInDays; // number of days till maturity
-        uint        startTime;      // time stamp to begin counting from
-    }
-
     // Contract status
-    bool                _isActive;
-    bool                _isComplete;
+    bool public         _isActive;
+    bool public         _isComplete;
 
-    // Participating accounts and addresses
-    address             _broker;
-    address             _buyer;
+    // Participating addresses and accounts
+    address public      _broker;
+    address public      _buyer;
+    address public      _seller;
     TradingAccount      _buyerAcct;
-    address             _seller;
     TradingAccount      _sellerAcct;
 
-    // Condition and underlier
+    // Underlier details
+    bytes32 public      _feedName;
+    uint public         _strikePrice;
+    uint public         _notional;
     PriceFeedApi        _underlier;
-    StrikeCondition     _condition;
-    uint                _notional;
+
+    // Maturity details
+    uint public         _maturityInDays;
+    uint public         _startTime;
 
     function FeedBackedCall() {
         _broker = msg.sender;
+        _isActive = false;
+        _isComplete = false;
     }
 
-    // Initialize with participants and condition
+    // Initialize with participants and terms
     function initialize(
         address seller,
         address buyer,
+        bytes32 providerName,
         bytes32 feedName,
-        uint    notional,
         uint    strikeToMarketRatio,
+        uint    notional,
         uint    maturityInDays) returns (bool) {
-
-        _isActive = false;
-        _isComplete = false;
 
         // Trading accounts
         _buyer = buyer;
-        _buyerAcct = TradingAccount(buyer);
         _seller = seller;
+        _buyerAcct = TradingAccount(buyer);
         _sellerAcct = TradingAccount(seller);
 
-        // If the broker is also the seller, authorize their trading account
-        // for twice the maturity period
-        authorizeTradingAccounts(maturityInDays);
+        // Authorize trading account of msg.sender
+        authorizeTradingAccounts(100);
 
-        _underlier = PriceFeedApi(named("ether-camp/price-feed"));
-        uint marketPrice = _underlier.getPrice(feedName);
-        uint strikePrice = (strikeToMarketRatio / 100) * marketPrice;
+        // Real-world provider: "ether-camp/price-feed"
+        _underlier = PriceFeedApi(named(providerName));
+        _feedName = feedName;
 
-        _condition = StrikeCondition(feedName,
-                                     strikePrice,
-                                     maturityInDays,
-                                     block.timestamp);
+        // Strike price relative to market price
+        _strikePrice = (strikeToMarketRatio / 100) * _underlier.getPrice(feedName);
         _notional = notional;
+
+        // Maturity relative to current block timestamp
+        _maturityInDays = maturityInDays;
+        _startTime = block.timestamp;
 
         return true;
     }
@@ -105,29 +69,32 @@ contract FeedBackedCall is nameRegAware {
     // Authorize trading accounts
     function authorizeTradingAccounts(uint buffer) returns (bool) {
         if (msg.sender == _buyer) {
-            _buyerAcct.authorize(this, _condition.maturityInDays + buffer);
+            _buyerAcct.authorize(this, _maturityInDays + buffer);
             return true;
         }
         if (msg.sender  == _seller) {
-            _sellerAcct.authorize(this, _condition.maturityInDays + buffer);
+            _sellerAcct.authorize(this, _maturityInDays + buffer);
             return true;
         }
         return false;
     }
 
-    // Any party can validate the contract
+    // Validate the contract in order to activate it
     function validate() returns (bool) {
         if (_isActive || _isComplete) {
             return true;
         }
-        // Need valid trading accounts
-        if (_seller == 0 || !_sellerAcct.isAuthorized(this)) {
+        // Authorize trading account of msg.sender. This is assumed to be
+        // the counterparty of the initializer of this contract.
+        authorizeTradingAccounts(100);
+
+        // Need two valid trading accounts
+        if (_buyer == 0 || _seller == 0 ||
+            !_buyerAcct.isAuthorized(this) ||
+            !_sellerAcct.isAuthorized(this)) {
             return false;
         }
-        // TODO: allow exercise of out of the money options
-        // if (_buyer == 0 || !_buyerAcct.isAuthorized(this)) {
-        //     return false;
-        // }
+
         _isActive = true;
         return true;
     }
@@ -146,23 +113,26 @@ contract FeedBackedCall is nameRegAware {
         return true;
     }
 
-    // If condition is met on maturity, allow the buyer to exercise the option
+    // On maturity, allow the buyer to exercise the option
     function exercise() returns (bool) {
         if (msg.sender != _buyer) {
             return false;
         }
-        // TODO: allow exercise of out of the money options
-        if (!isConditionMet()) {
+        if (!isMature()) {
             return false;
         }
-        // Transfer the difference
-        int spotPrice = int(_underlier.getPrice(_condition.feedName));
-        int difference = spotPrice - int(_condition.strikePrice);
-        if (difference < 0) {
-            return false;
-        }
-        _sellerAcct.withdraw(uint(difference) * _notional);
-        _buyerAcct.send(this.balance); // TODO: .deposit() if authorized
+
+        // The broker mops up any unclaimed balance
+        _broker.send(this.balance);
+
+        // Buyer claims the underlier at the strike price
+        _buyerAcct.withdraw(_strikePrice * _notional);
+        _sellerAcct.deposit.value(this.balance)();
+
+        // Seller provides the underlier at the spot price
+        uint spotPrice = _underlier.getPrice(_feedName);
+        _sellerAcct.withdraw(spotPrice * _notional);
+        _buyerAcct.deposit.value(this.balance)();
 
         _isActive = false;
         _isComplete = true;
@@ -171,29 +141,24 @@ contract FeedBackedCall is nameRegAware {
 
     // ===== Utility functions ===== //
 
-    function isMature() private returns (bool) {
-        uint timeElapsedInSecs = block.timestamp - _condition.startTime;
+    function isMature() returns (bool) {
+        uint timeElapsedInSecs = block.timestamp - _startTime;
         uint timeElapsedInDays = timeElapsedInSecs / 86400;
-        if (timeElapsedInDays >= _condition.maturityInDays) {
+        if (timeElapsedInDays >= _maturityInDays) {
             return true;
         } else {
             return false;
         }
     }
 
-    function isAtOrInTheMoney() private returns (bool) {
-        if (_underlier.getPrice(_condition.feedName) >= _condition.strikePrice) {
-            return true;
+    function getMoneyness() returns (int) {
+        uint spotPrice = _underlier.getPrice(_feedName);
+        if (spotPrice < _strikePrice) {
+            return -1;
+        } else if (spotPrice > _strikePrice) {
+            return 1;
         } else {
-            return false;
-        }
-    }
-
-    function isConditionMet() private returns (bool) {
-        if (isMature() && isAtOrInTheMoney()) {
-            return true;
-        } else {
-            return false;
+            return 0;
         }
     }
 }
